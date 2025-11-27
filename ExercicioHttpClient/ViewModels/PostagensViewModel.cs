aff8337fe9b8da4fb691b01b971512da7c4c8aa3
Roger-Paulo01 <rel.cliente.r@gmail.com>
2025-11-26 22:31:15 -0300

    Nova_View

--- ExercicioHttpClient/ViewModels/PostagensViewModel.cs
+++ ExercicioHttpClient/ViewModels/PostagensViewModel.cs
@@ -6,22 +6,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ExercicioHttpClient.ViewModels
 {
-    public class PostagensViewModel : ObservableObject
+    public  partial class PostagensViewModel : ObservableObject
     {
         [ObservableProperty]
         public int id;
         [ObservableProperty]
-        public string title;
+        public int userid;
         [ObservableProperty]
+        public string title;
+        [ObservableProperty]    
         public string body;
+        public ICommand CarregarPostagensCommand => new Command(async () => CarregaPostagens());
 
         public async void CarregaPostagens()
         {
             List<Postagem> Listar = new List<Postagem>();
             Listar = await new PostagensService().GetPostagens();
+            Id = Listar[0].Id;
+            Title = Listar[0].Title;
+            Body = Listar[0].Body;
         }
     }
 }
