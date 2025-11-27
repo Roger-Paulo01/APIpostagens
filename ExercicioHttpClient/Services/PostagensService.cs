a3eca2e2620a62cee0c3706f39b187b09e712510
Roger-Paulo01 <rel.cliente.r@gmail.com>
2025-11-26 21:23:19 -0300

    Alguma_aula

--- ExercicioHttpClient/Services/PostagensService.cs
+++ ExercicioHttpClient/Services/PostagensService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExercicioHttpClient.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace ExercicioHttpClient.Services
 {
-    internal class PostagensService
+    public class PostagensService
     {
+        async public Task <List<Postagem>>GetPostagens()
+        {
+            //Pedir uma informação para a API
+            //Pegar o dado, que veio da API
+            //Formatar o dado para que ele seja um objeto
+
+            List<Postagem> postagems = new List<Postagem>();
+            string URI = "https://jsonplaceholder.typicode.com/posts";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync(URI);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //É necessário Serializar e Desserializar a informação
+                string conteudo = await responseMessage.Content.ReadAsStringAsync();
+                postagems = JsonConvert.DeserializeObject<List<Postagem>>(conteudo);
+            }
+            return postagems;
+        }
     }
 }
